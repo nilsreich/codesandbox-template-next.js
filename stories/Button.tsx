37e9bcc9e416cc72a38b0ee37a5207ca568d19c3
nilsreich <nilsreich@gmail.com>
2022-07-20 19:51:48 +0000

    storybook testUpdate 9 files

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,22 +1,4 @@
-import React from 'react';
-import './button.css';
-
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
   /**
    * Optional click handler
@@ -27,21 +9,9 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ label }: ButtonProps) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button type="button" className="text-3xl">
       {label}
     </button>
   );
